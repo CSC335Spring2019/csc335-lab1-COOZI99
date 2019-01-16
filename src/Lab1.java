be0975a66260a5a9e4c5089d4d58098ac1c6a1fb
Bianca Lara <Bianca Lara@10.134.233.238>
2019-01-16 12:16:18 -0700

    Adding Author

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,13 +1,12 @@
 /**
  * 
- * @author YOUR NAME HERE
+ * @author Bianca Lara
  * 
  * 
  */
 public class Lab1 {
 
 	public static void main(String[] args) {
-		// TODO Change this to "Hello CSC 335, I'm YOURNAME"
 		System.out.println("Hello CSC 335, I'm Bianca Lara");
 	}
 
